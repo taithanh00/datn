dfbee1a9828499287979b1099a1f5cce1a145afd
karos <taithanh421@gmail.com>
2026-03-21 21:08:20 +0700

    first commit

--- datn/obj/Debug/net10.0/datn.AssemblyInfo.cs
+++ datn/obj/Debug/net10.0/datn.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("datn")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+195086e168c12ec9dbbd6bca42ffaa117e713361")]
 [assembly: System.Reflection.AssemblyProductAttribute("datn")]
 [assembly: System.Reflection.AssemblyTitleAttribute("datn")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
